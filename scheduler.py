5ff03b0ede40b603a0b003dd41f10bccbc332f6e
almazuulu <almazuulu@gmail.com>
2023-01-28 16:24:30 +0600

    Fixed with priority

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,7 @@
 import time
 import random
-
+from queue import PriorityQueue
+from collections import defaultdict
 
 class Resource:
     """
@@ -21,7 +22,7 @@ class Resource:
         """
         self.busy = True
         self.current_task = task
-        print(f"Resource {self.id} started task for project {task.project}")
+        print(f"Resource {self.id} started task for project {task.project} with priority {task.priority}")
 
     def end_task(self):
         """
@@ -29,37 +30,46 @@ class Resource:
         """
         if self.current_task:
             self.busy = False
-            print(f"Resource {self.id} ended task for project {self.current_task.project}")
+            print(f"Resource {self.id} ended task for project {self.current_task.project} with priority {self.current_task.priority}")
             self.current_task = None
-
 
 class Task:
     """
-    Class representing a task with a project.
+    Class representing a task with a project and priority.
     """
-    def __init__(self, project):
+    def __init__(self, project, priority):
         self.project = project
-
+        self.priority = priority
+        
+    def __lt__(self, other):
+        return self.priority < other.priority
 
 class OutsideInterface:
     """
     Class representing the interface for getting new tasks.
     """
     def __init__(self):
-        self.tasks = [Task("project1"), Task("project2"), Task("project3"), Task("project4"),
-                      Task("project5"), Task("project6"), Task("project7"), Task("project8"),
-                      Task("project9"), Task("project10")]
+        self.tasks = PriorityQueue()
+        self.tasks.put(Task("project1", 1))
+        self.tasks.put(Task("project2", 2))
+        self.tasks.put(Task("project3", 3))
+        self.tasks.put(Task("project4", 1))
+        self.tasks.put(Task("project5", 2))
+        self.tasks.put(Task("project6", 3))
+        self.tasks.put(Task("project7", 1))
+        self.tasks.put(Task("project8", 2))
+        self.tasks.put(Task("project9", 3))
+        self.tasks.put(Task("project10", 1))
 
     def check_for_new_task(self):
         """
         Check for a new task and return it if there is one.
         """
-        if self.tasks:
-            return self.tasks.pop(0)
+        if not self.tasks.empty():
+            return self.tasks.get()
         else:
             return None
-
-
+        
 class Scheduler:
     """
     Class that schedules tasks to resources.
@@ -71,35 +81,51 @@ class Scheduler:
         self.resources = resources
         self.outside_interface = outside_interface
         self.next_resource_index = 0
+        self.projects = defaultdict(lambda: {"count": 0})
+        for resource in resources:
+            self.projects[resource.id] = {"project": None, "count": 0}
 
     def run_scheduler(self):
         """
-        Run the scheduler continuously, getting new tasks and assigning them to resources.
+        Run the scheduler and assign tasks to resources.
         """
         while True:
             task = self.outside_interface.check_for_new_task()
-            if not task:
+            if task is None:
                 break
-            while True:
-                resource = self.resources[self.next_resource_index]
-                if not resource.busy:
-                    resource.start_task(task)
-                    break
-                else:
-                    self.next_resource_index = (self.next_resource_index + 1) % len(self.resources)
-                    time.sleep(0.1)
-                    continue
-                break
-            time.sleep(random.randint(1, 5)) # simulate task execution time
-            resource.end_task()
-            self.next_resource_index = (self.next_resource_index + 1) % len(self.resources)
+            resource = self.get_next_resource()
+            while resource.busy: # check if resource is busy
+                resource = self.get_next_resource() # get the next resource if current resource is busy
+            resource.start_task(task)
+            self.update_project_count(task.project)
+
+    def get_next_resource(self):
+        """
+        Get the next available resource.
+        """
+        resource = self.resources[self.next_resource_index]
+        self.next_resource_index += 1
+        if self.next_resource_index == len(self.resources):
+            self.next_resource_index = 0
+        return resource
+
+    def update_project_count(self, project_id):
+        """
+        Update the count of tasks for a given project.
+        """
+        self.projects[project_id]["count"] += 1
+        self.projects[project_id]["project"] = project_id
+
+    def get_project_count(self, project_id):
+        """
+        Get the count of tasks for a given project.
+        """
+        return self.projects[project_id]["count"]
 
 
-if __name__ == "__main__":
-    # Create two resources and an outside interface
+if __name__ == '__main__':
     resources = [Resource(i) for i in range(2)]
     outside_interface = OutsideInterface()
-
-    # Create a scheduler and run it
     scheduler = Scheduler(resources, outside_interface)
     scheduler.run_scheduler()
+
--- test_scheduler.py
+++ test_scheduler.py
@@ -1,13 +1,49 @@
 import unittest
-from scheduler import Resource, Scheduler, OutsideInterface, Task
+from scheduler import Scheduler, Task, Resource
 
 class TestScheduler(unittest.TestCase):
-    def test_run_scheduler(self):
-        resources = [Resource(i) for i in range(2)]
-        outside_interface = OutsideInterface()
-        scheduler = Scheduler(resources, outside_interface)
-        scheduler.run_scheduler()
-        self.assertEqual(1,1) 
+    def setUp(self):
+        self.scheduler = Scheduler()
+        self.resource1 = Resource("resource1")
+        self.resource2 = Resource("resource2")
+        self.task1 = Task("task1", "project1", 1, self.resource1)
+        self.task2 = Task("task2", "project2", 2, self.resource1)
+        self.task3 = Task("task3", "project3", 3, self.resource2)
+        self.task4 = Task("task4", "project4", 4, self.resource2)
+
+    def test_add_task(self):
+        self.scheduler.add_task(self.task1)
+        self.scheduler.add_task(self.task2)
+        self.scheduler.add_task(self.task3)
+        self.scheduler.add_task(self.task4)
+        self.assertEqual(len(self.scheduler.tasks), 4)
+
+    def test_add_resource(self):
+        self.scheduler.add_resource(self.resource1)
+        self.scheduler.add_resource(self.resource2)
+        self.assertEqual(len(self.scheduler.resources), 2)
         
-if __name__ == '__main__':
-    unittest.main()
+    def test_get_next_task(self):
+        self.scheduler.add_task(self.task1)
+        self.scheduler.add_task(self.task2)
+        self.scheduler.add_task(self.task3)
+        self.scheduler.add_task(self.task4)
+        next_task = self.scheduler.get_next_task()
+        self.assertEqual(next_task, self.task1)
+        
+    def test_run_scheduler(self):
+        self.scheduler.add_task(self.task1)
+        self.scheduler.add_task(self.task2)
+        self.scheduler.add_task(self.task3)
+        self.scheduler.add_task(self.task4)
+        self.scheduler.add_resource(self.resource1)
+        self.scheduler.add_resource(self.resource2)
+        self.scheduler.run_scheduler()
+        self.assertEqual(self.resource1.current_task, self.task1)
+        self.assertEqual(self.resource2.current_task, self.task2)
+        
+    def test_update_project_count(self):
+        self.scheduler.update_project_count("project1")
+        self.assertEqual(self.scheduler.projects["project1"]["count"], 1)
+        
+    
